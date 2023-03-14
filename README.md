24cb3bba183caaed5a80b4d77f75ab816277113e
John Rongved <john.rongved@gmail.com>
2023-03-14 14:21:37 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Virtual environment
+* https://realpython.com/python-virtual-environments-a-primer/
+
+## Install requirements
+* `pip install -r requirements.txt`
+
+## Run the app
+* `flask --app app run`
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+fruits = [
+    { 
+        'fruit': 'Mango', 
+        'amount': 2 
+    }
+]
+
+
+@app.route('/fruits')
+def get_incomes():
+    return jsonify(fruits)
+
+
+@app.route('/fruits', methods=['POST'])
+def add_income():
+    fruits.append(request.get_json())
+    return '', 204
